161b4826641e7170051abbea02c25bbd39ab8d1f
Maple <zhiyuzheng7@gmail.com>
2026-02-03 22:58:01 +0800

    Add ORFS-Research, Pin3D, and flow-Agent as submodules

--- 3DEval/platforms/nangate45/fastroute.tcl
+++
@@ -1,4 +0,0 @@
-set_global_routing_layer_adjustment M2-M3 0.5
-set_global_routing_layer_adjustment M4-$::env(MAX_ROUTING_LAYER) 0.25
-
-set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
--- 3DEval/platforms/nangate45_3D/fastroute.tcl
+++
@@ -1,1 +0,0 @@
-set_routing_layers -signal $::env(MIN_ROUTING_LAYER)-$::env(MAX_ROUTING_LAYER)
